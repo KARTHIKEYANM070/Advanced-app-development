ba3f67ee46b0d91d4dab1d952c3c26ecb859f23b
KARTHIKEYANM070 <125952230+KARTHIKEYANM070@users.noreply.github.com>
2024-02-15 12:34:26 +0530

    Add files via upload

---
+++ Integration/Backend/src/main/java/com/job_portal/online_job_portal/Controller/JobController.java
@@ -0,0 +1,68 @@
+package com.job_portal.online_job_portal.Controller;
+
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.job_portal.online_job_portal.Services.jobservice;
+import com.job_portal.online_job_portal.model.Job;
+// import com.job_portal.online_job_portal.model.SearchParams;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+@RestController
+@CrossOrigin
+ @RequestMapping("/addJobDetails")
+class EventController {
+
+    @Autowired
+     jobservice r;
+
+@PostMapping("/add/job")
+public String addDetails(@RequestBody Job m) {
+    r.add(m);
+    return "Job Added Successfully " ;
+}
+@Tag(name = "Get Users", description = "endpoint for fetching all users")
+@GetMapping("/get/job")
+public List<Job> showDetails() {
+    return r.getDetails();
+
+}
+@GetMapping("/get/job/{id}")
+public Job showDetail(@PathVariable int id) {
+    return r.getDetail(id);
+
+}
+
+
+
+@PutMapping("/update/job/{id}")
+public Job Details(@RequestBody Job newUser ,@PathVariable int id)
+{
+     return r.updateDetails(newUser,id);
+}
+
+@DeleteMapping("/delete/job/{id}")
+public String delete(@PathVariable int id )
+{
+    r.deleteDetails(id);
+    return "your item id "+ "is deleted";
+}
+// @PostMapping("/search")
+//     public List<Job> searchJobs(@RequestBody SearchParams searchParams) {
+//         return r.searchJobs(searchParams);
+//     }
+
+}
---
+++ Integration/Backend/src/main/java/com/job_portal/online_job_portal/model/SearchParams.java
@@ -0,0 +1,33 @@
+package com.job_portal.online_job_portal.model;
+
+public class SearchParams {
+    private String role;
+    private String type;
+    private String location;
+    private String experience;
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getExperience() {
+        return experience;
+    }
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+  
+}
