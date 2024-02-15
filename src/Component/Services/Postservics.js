e3604be0381a09442346a1784b1823b3c9e8e8c6
KARTHIKEYANM070 <125952230+KARTHIKEYANM070@users.noreply.github.com>
2024-02-15 12:38:26 +0530

    Delete src directory

--- src/Component/Services/Postservics.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import axios from "axios";
-const REST_API_BASE_URL='http://localhost:8080/addJobDetails/get/job';
-const Postservics = () => {
-  return axios.get(REST_API_BASE_URL);
-}
-
-export default Postservics
